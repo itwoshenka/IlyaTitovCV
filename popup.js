211075dfa96051b5e2f84c49e145f0be3a1dedab
itwoshenka <93199345+itwoshenka@users.noreply.github.com>
2022-09-11 04:25:47 +0300

    fix: popup!!!!
    finally fixed the popup with some cool features

--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
-const popupBtns = document.querySelectorAll('.submitbutton');
-const popupBg = document.getElementById('')
-const popupWindow = document.getElementById('')
-function popupAppear(){
-        popupBtns.addEventListener("click", function (e){
-            e.preventDefault();
-            alert("JAVASCRIIIPT!");
+const popupBtn = document.querySelectorAll('.submitbutton');
+const popupBg = document.querySelector('.popup_bg');
+const popupWindow = document.querySelector('.popup');
+const popupClose = document.querySelector('.okbutton')
+popupBtn.forEach((popupBtn) => {
+            popupBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+            popupBg.classList.add('active');
+            popupWindow.classList.add('active');
         })
+    });
+popupClose.addEventListener('click',() =>{
+    popupBg.classList.remove('active');
+    popupWindow.classList.remove('active');
+})
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popupWindow.classList.remove('active');
     }
-console.log(popupBtns)
+});
---
+++ scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".header_nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+function scroll(){
+    animate({scroll: top}, 1500)
+}
