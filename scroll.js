75475269bec1d5c3a1189101617cbe2a82a304c3
itwoshenka <93199345+itwoshenka@users.noreply.github.com>
2022-09-11 05:48:30 +0300

    fix: buttons on header
    now buttons on header are working! with javascript

--- scroll.js
+++ scroll.js
@@ -1,12 +1,42 @@
-$(document).ready(function(){
-    $(".header_nav").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
+const educationButton = document.getElementById('nav_education');
+const skillsButton = document.getElementById('nav_skills');
+const hobbiesButton = document.getElementById('nav_hobbies');
+const expButton = document.getElementById('nav_exp');
+const langsButton = document.getElementById('nav_langs');
+const contactButton = document.getElementById('nav_contact')
+educationButton.addEventListener('click', () => {
+    window.scroll({
+        top: 200,
+        behavior: "smooth"
+    })
 });
-
-function scroll(){
-    animate({scroll: top}, 1500)
-}
+skillsButton.addEventListener('click', () => {
+    window.scroll({
+        top: 550,
+        behavior: "smooth"
+    })
+});
+hobbiesButton.addEventListener('click', () => {
+    window.scroll({
+        top: 1050,
+        behavior: "smooth"
+    })
+});
+expButton.addEventListener('click', () => {
+    window.scroll({
+        top: 1300,
+        behavior: "smooth"
+    })
+});
+langsButton.addEventListener('click', () => {
+    window.scroll({
+        top: 1500,
+        behavior: "smooth"
+    })
+});
+contactButton.addEventListener('click', () => {
+    window.scroll({
+        top: 1800,
+        behavior: "smooth"
+    })
+});
