79a90eead8ab313f29661f0c20c5b0a97903f294
itwoshenka <93199345+itwoshenka@users.noreply.github.com>
2022-09-24 04:29:39 +0300

    feat: name show

--- placeholders.js
+++ placeholders.js
@@ -1,10 +1,9 @@
-const ph1 = document.getElementsByName('name')[0].placeholder="Enter your name...";
-const langSelect = document.getElementsByClassName('selection')
+const ph1 = document.getElementById('name');
+const langSelect = document.querySelector('selection')
 function changeLanguage(langSelect){
-langSelect.addEventListener('change', (e) => {
+langSelect.addEventListener('click', (e) => {
     e.preventDefault();
-    if (ph1.value==="Enter your name...") ph1.value==="Введите ваше имя";
-    else ph1.value==="Enter your name..."
+    ph1.placeholder==="Введите ваше имя";
+    refreshLabels();
 });
 };
-console.log(langSelect)
--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
 const popupBtn = document.querySelectorAll('.submitbutton');
 const popupBg = document.querySelector('.popup_bg');
 const popupWindow = document.querySelector('.popup');
-const popupClose = document.querySelector('.okbutton')
+const popupClose = document.querySelector('.okbutton');
+const popupText = document.querySelector('.popup_text');
 popupBtn.forEach((popupBtn) => {
             popupBtn.addEventListener('click', (e) => {
                 e.preventDefault();
@@ -19,3 +20,43 @@ document.addEventListener('click', (e) => {
         popupWindow.classList.remove('active');
     }
 });
+let inputEmail= document.getElementById("email");
+let inputName= document.getElementById("name");
+let inputMsg= document.getElementById("message");
+let formInputs = document.querySelectorAll('.input')
+let division = document.createElement('span');
+division.textContent= ','
+document.getElementById('name').oninput = () => {
+    document.querySelector('.userName').innerHTML = document.getElementById('name').value + division.textContent;
+}
+function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+form.onsubmit = function () {
+    let emailVal = inputEmail.value,
+        emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+
+    formInputs.forEach(function (input) {
+        if (input.value === '') {
+            input.classList.add('error');
+
+        } else {
+            input.classList.remove('error');
+        }
+    });
+
+    if (emptyInputs.length !== 0) {
+        console.log('inputs not filled');
+        return false;
+    }
+
+    if(!validateEmail(emailVal)) {
+        console.log('email not valid');
+        inputEmail.classList.add('error');
+        return false;
+    } else {
+        inputEmail.classList.remove('error');
+    }
+}
